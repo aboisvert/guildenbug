e2d5d063cdfdc9db14040d6c68bd9fcc9c6abc04
Alex Boisvert <alexb@ginger.io>
2020-12-05 08:43:19 -0800

    Code cleanup

--- guildenbug.nim
+++ guildenbug.nim
@@ -4,12 +4,16 @@ import uri
 from os import fileExists
 from strutils import removePrefix
 
+# WARNING - Contains code unsafe for production use.
+#           Will serve any local files!
+
 proc error(body: string, ctx: HttpCtx) =
+  ## Send back an error - HTTP/500
   echo "Error: " & body
   ctx.reply(Http500, body)
 
 proc serveFile(path: string, ctx: HttpCtx) =
-  echo "File: " & path
+  ## Serve a file back to the client
   if fileExists(path):
     let f = open(path)
     defer: f.close()
@@ -20,27 +24,28 @@ proc serveFile(path: string, ctx: HttpCtx) =
     ctx.reply(Http404, msg)
 
 proc serveDir(path: string, removePrefix: string, replacePrefix: string, ctx: HttpCtx) =
+  ## Serve files from a local directory tree
   var path = path
   path.removePrefix(removePrefix)
   serveFile(replacePrefix & path, ctx)
 
 proc handleHttpGet(uri: Uri, ctx: HttpCtx) =
   if uri.path == "/":
+    # serve index.html by default
     serveFile("index.html", ctx)
-  serveDir(uri.path, "/", "./", ctx)
+  else:
+    # otherwise, serve any local files from current directory
+    serveDir(uri.path, "/", "./", ctx)
 
 proc handleHttpRequest*(ctx: HttpCtx, headers: StringTableRef) {.gcsafe, raises: [].} =
-  {.gcsafe.}:
-    try:
-      echo "handleHttpRequest"
-      let uri = ctx.getUri().parseUri()
-      echo "uri: " & $uri
-      if ctx.getMethod() == "GET": handleHttpGet(uri, ctx)
-      else: error("Unexpected method: " & ctx.getMethod(), ctx)
-    except:
-      let msg = getCurrentExceptionMsg()
-      error(msg, ctx)
-    ctx.closeSocket()
+  try:
+    let uri = ctx.getUri().parseUri()
+    if ctx.getMethod() == "GET": handleHttpGet(uri, ctx)
+    else: error("Unexpected method: " & ctx.getMethod(), ctx)
+  except:
+    let msg = getCurrentExceptionMsg()
+    error(msg, ctx)
+  ctx.closeSocket()
 
 var server = new GuildenServer
 server.initFullCtx(handleHttpRequest, 8080)
